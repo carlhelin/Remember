a2d5230fb222ee56f961cff1bbe44532e4620d4c
carlthe <carlthe@uio.no>
2022-07-26 20:12:47 +0930

    Initial Commit

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Carl Helin on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
